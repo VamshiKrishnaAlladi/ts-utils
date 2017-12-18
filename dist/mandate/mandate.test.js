466784c0c74db728a6dc95bb4dc053a245bb85c7
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-18 14:50:23 +0530

    Added test for the functionality of the "mandate" utility

--- dist/mandate/mandate.test.js
+++ dist/mandate/mandate.test.js
@@ -1,6 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var missing_mandatory_param_1 = require("./../errors/missing-mandatory-param");
 describe('Module ts-utils/mandate:', function () {
     test('should export a function "mandate"', function () {
         var mandate = require('./mandate').mandate;
         expect(mandate).toBeInstanceOf(Function);
+    });
+    describe('Exported function "mandate":', function () {
+        test('should throw a MissingMandatoryParamError when called', function () {
+            var mandate = require('./mandate').mandate;
+            expect(mandate).toThrowError(missing_mandatory_param_1.MissingMandatoryParamError);
+        });
     });
 });
--- mandate/mandate.test.ts
+++ mandate/mandate.test.ts
@@ -1,7 +1,17 @@
+import { MissingMandatoryParamError } from './../errors/missing-mandatory-param';
+
 describe('Module ts-utils/mandate:', () => {
     test('should export a function "mandate"', () => {
         const { mandate } = require('./mandate');
 
         expect(mandate).toBeInstanceOf(Function);
+    });
+
+    describe('Exported function "mandate":', () => {
+        test('should throw a MissingMandatoryParamError when called', () => {
+            const { mandate } = require('./mandate');
+    
+            expect(mandate).toThrowError(MissingMandatoryParamError);
+        });
     });
 });
