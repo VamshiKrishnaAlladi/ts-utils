c0ab690d05c0e77462aa52ec7f039810187792cd
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-18 13:10:58 +0530

    Removed unnecessary test

--- dist/ts-utils.test.js
+++ dist/ts-utils.test.js
@@ -1,8 +1,4 @@
 describe('Module TS Utils:', function () {
-    test('should export an object', function () {
-        var utils = require('./ts-utils');
-        expect(utils).toBeInstanceOf(Object);
-    });
     test('should expose the function "mandate"', function () {
         var utils = require('./ts-utils');
         expect(utils).toHaveProperty('mandate');
--- ts-utils.test.ts
+++ ts-utils.test.ts
@@ -1,10 +1,4 @@
 describe('Module TS Utils:', () => {
-    test('should export an object', () => {
-        const utils = require('./ts-utils');
-
-        expect(utils).toBeInstanceOf(Object);
-    });
-
     test('should expose the function "mandate"', () => {
         const utils = require('./ts-utils');
 
