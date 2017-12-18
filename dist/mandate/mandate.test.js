1d65fe79b932434519415d4d0bbe2a485da6cec4
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-18 13:49:27 +0530

    Changed the test suite description.

--- dist/mandate/mandate.test.js
+++ dist/mandate/mandate.test.js
@@ -1,5 +1,5 @@
-describe('Module mandate:', function () {
-    test('should export a function', function () {
+describe('Module ts-utils/mandate:', function () {
+    test('should export a function "mandate"', function () {
         var mandate = require('./mandate').mandate;
         expect(mandate).toBeInstanceOf(Function);
     });
--- mandate/mandate.test.ts
+++ mandate/mandate.test.ts
@@ -1,5 +1,5 @@
-describe('Module mandate:', () => {
-    test('should export a function', () => {
+describe('Module ts-utils/mandate:', () => {
+    test('should export a function "mandate"', () => {
         const { mandate } = require('./mandate');
 
         expect(mandate).toBeInstanceOf(Function);
