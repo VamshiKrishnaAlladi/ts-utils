6f165b51b1dd6d51e409ec16257d498a1e73e275
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-29 19:57:03 +0530

    Deleted all compiled .test.js and .test.d.ts files

--- ts-utils.test.js
+++
@@ -1,7 +0,0 @@
-describe('Module TS Utils:', function () {
-    test('should expose the function "mandate"', function () {
-        var utils = require('./ts-utils');
-        expect(utils).toHaveProperty('mandate');
-        expect(utils.mandate).toBeInstanceOf(Function);
-    });
-});
