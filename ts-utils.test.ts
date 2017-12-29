98385c9afb37f8866bd2843769cca5bfb20592c7
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-29 22:02:39 +0530

    Deleted ts-utils.test.ts
    The tests are pointless for a file that just relays the exports from other sub-modules

--- ts-utils.test.ts
+++
@@ -1,8 +0,0 @@
-describe('Module TS Utils:', () => {
-    test('should expose the function "mandate"', () => {
-        const utils = require('./ts-utils');
-
-        expect(utils).toHaveProperty('mandate');
-        expect(utils.mandate).toBeInstanceOf(Function);
-    });
-});
