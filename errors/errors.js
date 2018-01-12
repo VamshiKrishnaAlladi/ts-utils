505a298ad28f95e92b3414bdfc98264bb39802b8
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-01-12 21:26:07 +0530

    Relaying the exports of errors/forbidden-action-error via errors.ts

--- errors/errors.js
+++ errors/errors.js
@@ -5,3 +5,4 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./base-error"));
 __export(require("./missing-mandatory-param-error"));
+__export(require("./forbidden-action-error"));
