0129f6e5ed582be2990eb2ee0e69db5013eca7ef
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-01-12 21:30:14 +0530

    Importing from the sub-module instead of importing directly from the implementation file

--- mandate/mandate.test.ts
+++ mandate/mandate.test.ts
@@ -2,20 +2,20 @@ import { MissingMandatoryParamError } from './../errors';
 
 describe('Module ts-utils/mandate:', () => {
     test('should export a function "mandate"', () => {
-        const { mandate } = require('./mandate');
+        const { mandate } = require('.');
 
         expect(mandate).toBeInstanceOf(Function);
     });
 
     describe('Exported function "mandate":', () => {
         test('should throw a MissingMandatoryParamError when called', () => {
-            const { mandate } = require('./mandate');
+            const { mandate } = require('.');
 
             expect(mandate).toThrowError(MissingMandatoryParamError);
         });
 
         test('should specify the paramName passed in the MissingMandatoryParamError thrown', () => {
-            const { mandate } = require('./mandate');
+            const { mandate } = require('.');
 
             expect(() => { mandate('someParam');}).toThrowError(/.*"someParam"/);
         });
